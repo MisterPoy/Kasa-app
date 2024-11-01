27303b94c7ae9e8eab9889747223501a8f0ed4b2
MrPoy <gregory.poupaux@hotmail.fr>
2024-11-01 14:56:54 +0100

    update carousel
    Added fade animation on image change.
    Added a subtle shadow to the counter and arrows for better visibility on very light or white images.

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -3,25 +3,39 @@ import "./carousel.scss";
 
 export function Carousel({ pictures, alt }) {
   const [currentIndex, setCurrentIndex] = useState(0); // state pour suivre l'index
+  const [fade, setFade] = useState(false); // state pour gérer l'animation de fondu
 
   // methode pour afficher l'image précédente
   function previousImg() {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
-    );
+    setFade(true); // active le fondu de l'image actuelle
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+      );
+      setFade(false);
+    }, 350); // durée de l'animation
   }
   // methode pour afficher l'image suivante
   function nextImg() {
-    setCurrentIndex((nextIndex) =>
-      nextIndex === pictures.length - 1 ? 0 : nextIndex + 1
-    );
+    setFade(true);
+    setTimeout(() => {
+      setCurrentIndex((nextIndex) =>
+        nextIndex === pictures.length - 1 ? 0 : nextIndex + 1
+      );
+      setFade(false);
+    }, 350); // durée de l'animation
   }
 
   return (
     <div
       className="carousel"
-      style={{ backgroundImage: `url(${pictures[currentIndex]})` }}
+      /* style={{ backgroundImage: `url(${pictures[currentIndex]})` }} */
     >
+      <div
+        className={`carousel-img ${fade ? "fade" : ""}`}
+        style={{ backgroundImage: `url(${pictures[currentIndex]})` }}
+        alt={alt}
+      ></div>
       {pictures.length > 1 && (
         <>
           <div>
@@ -29,7 +43,7 @@ export function Carousel({ pictures, alt }) {
               onClick={previousImg}
               src="/src/assets/arrowLeft.png"
               alt="Arrow left"
-              className="arrowLeft"
+              className="arrow arrowLeft"
             />
           </div>
           <div>
@@ -37,7 +51,7 @@ export function Carousel({ pictures, alt }) {
               onClick={nextImg}
               src="/src/assets/arrowRight.png"
               alt="Arrow right"
-              className="arrowRight"
+              className="arrow arrowRight"
             />
           </div>
           <div className="img-count">{`${currentIndex + 1}/${
