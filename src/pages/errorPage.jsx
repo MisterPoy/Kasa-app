cd9bcb99f4d59c0769572e6baf8352ff1a07fde2
MrPoy <gregory.poupaux@hotmail.fr>
2024-11-04 17:03:10 +0100

    responsive OK
    Responsive OK

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -4,14 +4,15 @@ import "./errorPage.scss";
 
 export function ErrorPage() {
   return (
-    <>
-      <span className="txt404">404</span>
-      <h2 className="title-error">
-        Oups! La page que vous demandez n&apos;existe pas.
-      </h2>
-      <Link to="/" className="returnHome">
-        Retourner sur la page d&apos;acceuil
-      </Link>
-    </>
+    <div className="error-page-container">
+        <span className="txt404">404</span>
+        <h2 className="title-error">
+          Oups! La page que vous demandez n&apos;existe pas.
+        </h2>
+        <Link to="/" className="returnHome">
+          Retourner sur la page d&apos;acceuil
+        </Link>
+    </div>
+    
   );
 }
